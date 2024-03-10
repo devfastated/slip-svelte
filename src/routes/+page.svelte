2fdd1b916dd7056dfab5f93973a7b8824521fb64
Romer Jasen Jimenez <businessmailromer@gmail.com>
2024-03-10 22:47:38 +0800

    second commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+     let count = 0;
+     let countminus = 0;
+     let name = "Romer Jasen";
+</script>
+
+<button on:click={() => count++}> count is Count is {count} <br /> </button>
+{name}
+
+<button on:click={() => countminus--}>
+     count is Count is {countminus} <br />
+</button>
+{name}
